9ffbb699f6be1acc8f6df2c526f2aabd367b1c8b
Cem Olcay <ccemolcay@gmail.com>
2015-06-11 11:11:33 +0300

    Initial Commit

---
+++ YSTutorialViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YSTutorialViewController
+//
+//  Created by Cem Olcay on 11/06/15.
+//  Copyright (c) 2015 Cem Olcay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YSTutorialViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YSTutorialViewController
+//
+//  Created by Cem Olcay on 11/06/15.
+//  Copyright (c) 2015 Cem Olcay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
